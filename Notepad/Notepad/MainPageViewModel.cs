6c7be5750940ecd9844cad40e0db61ccd75e974c
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-03-30 23:07:19 +0300

    Добавьте файлы проекта.

---
+++ Notepad/Notepad/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace Notepad
+{
+    class MainPageViewModel : ViewModelBase
+    {
+        private string appTitle;
+        public string AppTitle { get => appTitle; set => SetProperty(ref appTitle, value); }
+        public MainPageViewModel()
+        {
+            AppTitle = "Notepad";
+        }
+    }
+}
