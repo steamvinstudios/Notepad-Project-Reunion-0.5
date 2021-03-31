7d3808b3af48742b70925dd3dc1b6ca1a12b6f6d
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-03-31 22:05:26 +0300

    Added List of notes

---
+++ Notepad/Notepad/AppStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using Microsoft.Win32;
+using Windows.Storage;
+
+namespace Notepad
+{
+    public class AppStorage
+    {
+        static string textTmp;
+        public AppStorage(string text)
+        {
+
+        }
+    }
+}
--- Notepad/Notepad/MainPageViewModel.cs
+++ Notepad/Notepad/MainPageViewModel.cs
@@ -10,7 +10,9 @@ namespace Notepad
 {
     class MainPageViewModel : ViewModelBase
     {
+        public List<string> Notes { get; set; } = new List<string>();
         public ICommand ChangeTextCommand { get; set; }
+        public ICommand OpenFolderCommand { get; set; }
         private string appTitle;
         public string AppTitle { get => appTitle; set => SetProperty(ref appTitle, value); }
         public MainPageViewModel()
